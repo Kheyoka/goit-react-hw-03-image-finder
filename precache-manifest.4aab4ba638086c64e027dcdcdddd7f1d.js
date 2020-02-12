2d71813db434e57ed9060fa2be2bc1c7609c3882
Kseniya G Vo <vwk8888@gmail.com>
2020-02-12 20:19:59 +0200

    Updates

---
+++ precache-manifest.4aab4ba638086c64e027dcdcdddd7f1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0412cd179a085c1b0a1525c2cab029",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "019e6ede6a423804e98e",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.48fd4f27.chunk.css"
+  },
+  {
+    "revision": "f230c2a43fc9529a966c",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.1178b148.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.1178b148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019e6ede6a423804e98e",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.12480afb.chunk.js"
+  },
+  {
+    "revision": "9ed00262ab067fcfa080",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.5a6095d3.js"
+  }
+]);
